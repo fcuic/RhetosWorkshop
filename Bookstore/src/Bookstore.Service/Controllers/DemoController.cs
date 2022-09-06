7c38ca5b5c947a2e2c22f317426c61a5d8f2fabc
fcuic <fcuic@cmbetsolutions.com>
2022-09-06 13:15:50 +0200

    Initial Commit, finished day 1

---
+++ Bookstore/src/Bookstore.Service/Controllers/DemoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rhetos;
+using Rhetos.Processing;
+using Rhetos.Processing.DefaultCommands;
+
+namespace Bookstore.Service.Controllers
+{
+    [Route("Demo/[action]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly IProcessingEngine _processingEngine;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly Common.ExecutionContext _executionContext;
+
+        public DemoController(IRhetosComponent<IProcessingEngine> processingEngine, IRhetosComponent<IUnitOfWork> unitOfWork, IRhetosComponent<Common.ExecutionContext> executionContext)
+        {
+            _processingEngine = processingEngine.Value;
+            _unitOfWork = unitOfWork.Value;
+            _executionContext = executionContext.Value;
+        }
+
+        [HttpGet]
+        public string ReadBooks() 
+        {
+            var readCommandInfo = new ReadCommandInfo { DataSource = "Bookstore.Book", ReadTotalCount = true };
+            var result = _processingEngine.Execute(readCommandInfo);
+            return $"{result.TotalCount} books.";
+        }
+
+        [HttpGet]
+        public string WriteBook() 
+        {
+            var newBook = new Bookstore.Book { Title = "NewBook" };
+            var saveCommandInfo = new SaveEntityCommandInfo { Entity = "Bookstore.Book", DataToInsert = new[] { newBook } };
+            _processingEngine.Execute(saveCommandInfo);
+            _unitOfWork.CommitAndClose();
+            return "1 book inserted.";
+        }
+
+        //My testing of querying from the db (Not mandatory)
+        [HttpGet]
+        public List<string> GetChildrenBooksNames() 
+        {
+            var childrenBooks = _executionContext.Repository.Bookstore.ChildrensBook.Query()
+                .Select(x => x.Base.Title).ToList();
+            return childrenBooks;
+        }
+
+    }
+}
---
+++ Bookstore/src/Bookstore.Service/Program.cs
@@ -0,0 +1,55 @@
+using Rhetos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+void ConfigureRhetosHostBuilder(IServiceProvider serviceProvider, IRhetosHostBuilder rhetosHostBuilder)
+{
+    rhetosHostBuilder
+        .ConfigureRhetosAppDefaults()
+        .UseBuilderLogProviderFromHost(serviceProvider)
+        .ConfigureConfiguration(cfg => cfg.MapNetCoreConfiguration(builder.Configuration));
+}
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o => o.CustomSchemaIds(type => type.ToString())); // CustomSchemaIds allows multiple entities with the same name in different modules.;
+builder.Services.AddRhetosHost(ConfigureRhetosHostBuilder)
+    .AddAspNetCoreIdentityUser()
+    .AddHostLogging()
+    .AddRestApi(o =>
+    {
+        o.BaseRoute = "rest";
+        o.GroupNameMapper = (conceptInfo, controller, oldName) => "v1";
+    });
+
+builder.Services
+    .AddRhetosHost()
+    .AddDashboard()
+    ;
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseRhetosRestApi();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment()) { 
+    app.MapRhetosDashboard();
+}
+
+app.Run();
+
+
